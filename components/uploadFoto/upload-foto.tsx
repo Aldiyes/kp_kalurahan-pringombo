a934825d00db8260d5b929148c73b19000e47e86
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-06-01 05:08:04 +0700

    uploadthing

--- components/uploadFoto/upload-foto.tsx
+++ components/uploadFoto/upload-foto.tsx
@@ -1,25 +1,25 @@
-"use client";
+'use client';
 
-import toast from "react-hot-toast";
+import toast from 'react-hot-toast';
 
-import { UploadDropzone } from "@/lib/uploadthing";
-import { ourFileRouter } from "@/app/api/uploadthing/core";
-
+import { ourFileRouter } from '@/app/api/uploadthing/core';
+import { UploadDropzone } from '@/lib/uploadthing';
 
 interface FileUploadProps {
-  onchange: (url?: string) => void;
-  endpoint: keyof typeof ourFileRouter;
+	onchange: (url?: string) => void;
+	endpoint: keyof typeof ourFileRouter;
 }
 
 export const FileUpload = ({ onchange, endpoint }: FileUploadProps) => {
-  return (
-    <UploadDropzone
-      endpoint={endpoint}
-      onClientUploadComplete={(res) => {
-        onchange(res?.[0].url);
-      }}
-      onUploadError={(error: Error) => {
-        toast.error('${error?.message}');}}
-    />
-  );
+	return (
+		<UploadDropzone
+			endpoint={endpoint}
+			onClientUploadComplete={(res) => {
+				onchange(res?.[0].url);
+			}}
+			onUploadError={(error: Error) => {
+				toast.error(`${error?.message}`);
+			}}
+		/>
+	);
 };
